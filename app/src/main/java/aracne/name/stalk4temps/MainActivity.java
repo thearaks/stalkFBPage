47e3de8fac76777ba6d47d52280f46396995a70f
Mattia Aracne <thearaks@gmail.com>
2017-09-08 00:14:21 +0200

    second commit

--- app/src/main/java/aracne/name/stalk4temps/MainActivity.java
+++ app/src/main/java/aracne/name/stalk4temps/MainActivity.java
@@ -1,18 +1,55 @@
 package aracne.name.stalk4temps;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    public static AccessToken accessToken;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+        accessToken = AccessToken.getCurrentAccessToken();
+        startButton = (Button) findViewById(R.id.start);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                startButton.setEnabled(true);
+                accessToken = loginResult.getAccessToken();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, StalkService.class);
                 startService(i);
--- app/src/main/java/aracne/name/stalk4temps/StalkService.java
+++ app/src/main/java/aracne/name/stalk4temps/StalkService.java
@@ -2,37 +2,21 @@ package aracne.name.stalk4temps;
 
 import android.app.Service;
 import android.content.Intent;
-import android.net.http.AndroidHttpClient;
 import android.os.Handler;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 
-import com.google.gson.Gson;
-
-import org.apache.http.HttpRequest;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.message.BasicHeader;
-
-import javax.net.ssl.HttpsURLConnection;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 
 
 public class StalkService extends Service {
 
     private String lastPostId;
     private Handler handler = new Handler();
-    private Runnable checkLastPost = new Runnable() {
-        @Override public void run() {
-            HttpRequest httpRequest = new HttpRequest() {
-            };
+    private Runnable checkLastPost = new CheckPostRunnable();
 
-
-            handler.post(checkLastPost);
-        }
-    };
 
     @Nullable @Override public IBinder onBind(Intent intent) {
         return null;
@@ -48,24 +32,21 @@ public class StalkService extends Service {
     }
 
     private class CheckPostRunnable implements Runnable {
-        private Gson gson;
-        private HttpClient client;
-        private HttpUriRequest request;
 
-        public CheckPostRunnable() {
-            gson = new Gson();
-            client = new AndroidHttpClient();
-
-            request = new HttpGet("https://graph.facebook.com/lesquatretemps/posts");
-            request.addHeader(new BasicHeader("Authentication", "Bearer " + "EAACEdEose0cBAOtZBtSWiHoI15DsaguVh5j6mxMGHAxEZBYDysrZCrx7yZBapkPPKy8yj6eAgeIPDPPmgKi1DqcY9BUkrkPZCaafCMhoSTrmUhVeUlAQrntU0RJtxofOT5WN5ZBzF6ZBT57pJhxVzeG7WHWVnQM4qa8hHUCnw5ZBxmWOa3RfreNvQxz5IoZCtcBUZD"));
-        }
+        public CheckPostRunnable() {}
 
         @Override public void run() {
-            String result = client.execute(request);
+            GraphRequest.newGraphPathRequest(MainActivity.accessToken, "lesquatretemps/posts", new GraphRequest.Callback() {
+                @Override
+                public void onCompleted(GraphResponse response) {
+                    if (response.getError() != null) {
+                        response.getJSONObject();
+                    }
 
-            gson.fromJson(result);
-
-            handler.postDelayed(checkLastPost, 60 * 1000);
+                    handler.postDelayed(checkLastPost, 3 * 60 * 1000);
+                }
+            }).executeAsync();
         }
     }
+
 }
